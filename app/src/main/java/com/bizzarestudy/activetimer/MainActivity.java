8dd588c7e3d4cccb8ff3def6fecc3b0a07d712a4
KURIET <prospersj777@gmail.com>
2020-05-07 14:48:03 +0900

    Runnable 없애고 Thread run 정의하는 방식으로 변경
    Time zone에 따라 밀리세컨드 더해주는 조정 필요 (현재는 한국기준으로 동작함)
    (UTC+09)

--- app/src/main/java/com/bizzarestudy/activetimer/MainActivity.java
+++ app/src/main/java/com/bizzarestudy/activetimer/MainActivity.java
@@ -11,6 +11,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -34,83 +36,131 @@ public class MainActivity extends AppCompatActivity {
     private static Handler mHandler;
 
     long start_Time = System.currentTimeMillis();
-    long store_Time;
+    long store_Time, previous_diff_Time_long = 0, current_diff_Time_long = 0;
+    boolean flag = true;
 
-    Thread t, t2;
+    Thread t;
+    Button start, stop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Connect UI Components
         active_Time = findViewById(R.id.active_time);
+        start = findViewById(R.id.start);
+        stop = findViewById(R.id.stop);
 
+        //Intent Filter to detect events
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(Intent.ACTION_SCREEN_ON);
         intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
 
-        tagHere("Current time : " + start_Time);
+        tagHere("===========App initalized time : " + start_Time);
 
+        //Handler for working on Main Thread
         mHandler = new Handler() {
-            @SuppressLint("HandlerLeak")
             @Override
             public void handleMessage(Message msg) {
                 store_Time = System.currentTimeMillis();
                 SimpleDateFormat sdf = new SimpleDateFormat("HH : mm : ss");
-                String diff_Time = sdf.format(store_Time - start_Time - 32400000);
+                // 68400000(USA) / 32400000(KOREA)
+                // 우리 시간(UTF+09)에 맞추어진 숫자의 차이
+                // 이전에 저장되었던 시간을 ------ diff_Time_long
+                // 00:00:00(UTF+09) 기준으로 ------ diff_Time_long - 32400000
+                // 딱 한번 더해준다.
+                previous_diff_Time_long = store_Time - start_Time;
+                tagHere("previous diff time long : " + previous_diff_Time_long);
+                //그리고 시간을 표시한다.
+                String diff_Time = sdf.format(current_diff_Time_long + store_Time - start_Time - 32400000);
                 active_Time.setText(diff_Time);
             }
         };
 
-        class NewRunnable implements Runnable {
+        t = new customThread();
+        t.start();
+
+        stop.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                while (true) {
+            public void onClick(View v) {
+                t.interrupt();
+                current_diff_Time_long = current_diff_Time_long + previous_diff_Time_long;
+                tagHere("stop button");
+            }
+        });
 
-                    try {
-                        Thread.sleep(1000);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start_Time = System.currentTimeMillis();
+                t = new customThread();
+                tagHere("!t.isAlive() = " + !t.isAlive());
+                if(!t.isAlive()) {
+                    t.start();
+                    tagHere("start button");
+                }
+            }
+        });
 
-                    mHandler.sendEmptyMessage(0);
+        this.registerReceiver(screenOnOff, intentFilter);
+
+
+    }
+
+    class customThread extends Thread {
+        @Override
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                mHandler.sendEmptyMessage(0);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    tagHere("interrupted");
+                    e.printStackTrace();
+                    break;
                 }
             }
         }
+    }
 
-        NewRunnable nr = new NewRunnable();
-        t = new Thread(nr);
-        t.start();
+    BroadcastReceiver screenOnOff = new BroadcastReceiver() {
+
+        public static final String ScreenOn = "android.intent.action.SCREEN_ON";
+        public static final String ScreenOff = "android.intent.action.SCREEN_OFF";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(ScreenOff)) {
+                //time stops
+
+                t.interrupt();
+                current_diff_Time_long = current_diff_Time_long + previous_diff_Time_long;
+                tagHere("SAME FUNCTION AS - stop button");
+
+                tagHere("AFTER stop --t.isAlive() = "+t.isAlive());
+                tagHere("t.isInterrupted() = "+t.isInterrupted());
 
 
-        BroadcastReceiver screenOnOff = new BroadcastReceiver() {
+            } else if (intent.getAction().equals(ScreenOn)) {
+                //time goes on
 
-            public static final String ScreenOn = "android.intent.action.SCREEN_ON";
-            public static final String ScreenOff = "android.intent.action.SCREEN_OFF";
-
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                if (intent.getAction().equals(ScreenOff)) {
-                    //time stops
-                    t.interrupt();
-
-                } else if (intent.getAction().equals(ScreenOn)) {
-                    //time goes on
-
+                start_Time = System.currentTimeMillis();
+                t = new customThread();
+                tagHere("!t.isAlive() = " + !t.isAlive());
+                if(!t.isAlive()) {
+                    t.start();
+                    tagHere("SAME FUNCTION AS - start button");
                 }
+
+                tagHere("go");
+                tagHere("t.isInterrupted() = "+t.isInterrupted());
+                tagHere("t.isAlive() = "+t.isAlive());
+                flag = true;
+
             }
-        };
-
-        this.registerReceiver(screenOnOff, intentFilter);
-    }
-
-    public void timerStop() {
-
-    }
-
-    public void timerStart() {
-
-    }
+        }
+    };
 
     public void tagHere(String message) {
         Log.i("sj", message);
